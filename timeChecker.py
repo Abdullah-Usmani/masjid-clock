e7ee7e8796f694de14b653178edff54c0438016a
Abdullah-Usmani <abdullahkangaroo8@gmail.com>
2024-02-26 11:27:31 +0800

    checker to current date's prayer times

--- timeChecker.py
+++ timeChecker.py
@@ -15,8 +15,6 @@ dataFrame = pandas.read_csv(r'C:\Users\Abdullah Usmani\Documents\MasjidClock\ath
 relevant_columns = ['Date', 'Hijri', 'Day', 'Imsak', 'Fajr', 'Syuruk', 'Zohor', 'Asar', 'Maghrib', 'Isyak']
 dataFrame = dataFrame[relevant_columns]
 
-selectedRow = 0
-
 currentTimeUTC = time.gmtime()  # Get current time in UTC
 currentTimeMYT = time.localtime(time.mktime(currentTimeUTC) + 8 * 3600)  # Add 8 hours for GMT+8
 
@@ -27,15 +25,58 @@ def dateComparer():
         i += 1
         if rowDate == currentDate:
             print(f"VOILA!\n - MATCH!, this is at row # {i}, the row's date is {rowDate}")
-            selectedRow == i
+            return i  # Return the selected row index
             break
 
-dateComparer()
+def timeComparer(selectedRow):
+    currentTime = datetime.datetime.now().time()  # Get current time as a datetime object
+    displayCurrentTime = datetime.datetime.now().strftime('%I:%M %p')  # Format current time as HH:MM:SS AM/PM
+    
+    fajrTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Fajr'], '%I:%M %p').time()
+    displayFajrTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Fajr'], '%I:%M %p').strftime('%I:%M %p')
+    
+    syurukTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Syuruk'], '%I:%M %p').time()
+    displaySyurukTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Syuruk'], '%I:%M %p').strftime('%I:%M %p')
+    
+    zohorTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Zohor'], '%I:%M %p').time()
+    displayZohorTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Zohor'], '%I:%M %p').strftime('%I:%M %p')
+    
+    asarTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Asar'], '%I:%M %p').time()
+    displayAsarTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Asar'], '%I:%M %p').strftime('%I:%M %p')
+    
+    maghribTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Maghrib'], '%I:%M %p').time()
+    displayMaghribTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Maghrib'], '%I:%M %p').strftime('%I:%M %p')
+    
+    isyakTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Isyak'], '%I:%M %p').time()
+    displayIsyakTime = datetime.datetime.strptime(dataFrame.loc[selectedRow, 'Isyak'], '%I:%M %p').strftime('%I:%M %p')
+    
 
-def timeComparer():
-    currentTime = time.strftime('%d-%b-%y', currentTimeMYT)
-    if currentTime < columnTime:
+    if currentTime < syurukTime:
+        print(f"{displayCurrentTime} vs {displaySyurukTime}")
         print("FAJR RIGHT NOW!")
+    elif currentTime < zohorTime:
+        print(f"{displayCurrentTime} vs {displayZohorTime}")
+        print("NOTHING RIGHT NOW")
+
+    elif currentTime < asarTime:
+        print(f"{displayCurrentTime} vs {displayAsarTime}")
+        print("ZOHOR RIGHT NOW")
+
+    elif currentTime < maghribTime:
+        print(f"{displayCurrentTime} vs {displayMaghribTime}")
+        print("ASAR RIGHT NOW")
+
+    elif currentTime < isyakTime:
+        print(f"{displayCurrentTime} vs {displayIsyakTime}")
+        print("MAGHRIB RIGHT NOW")
+
+    elif currentTime < fajrTime:
+        print(f"{displayCurrentTime} vs {displayFajrTime}")
+        print("ISYAK RIGHT NOW")
+
+selectedRow = dateComparer()
+timeComparer(selectedRow)
+
 
 
         # parsedRowDate = time.strptime(f'{rowDate}', '%d-%b-%y')
