ec629c68235ffaca44fe3b7acc8f51c190ccd0fe
Abdullah-Usmani <abdullahkangaroo8@gmail.com>
2024-09-03 02:55:02 +0500

    Iqama times, leading 0 zero removed

--- GUI.py
+++ GUI.py
@@ -1,12 +1,14 @@
 # Import Module
 from tkinter import *
 from timeChecker import *
-import customtkinter as ctk
 from PIL import Image
+import customtkinter as ctk
 
-df, prayer_headers, currentMYT = initializers(8)
+# %%
+df, prayer_headers, iqama_headers, currentMYT = initializers(8)
 selectedRow, date, hijri, day, headers, foundFlag = dateComparer(df, prayer_headers, currentMYT)
-currentTime, displayCurrentTime, act_times, display_times, current_prayer, next_prayer= timeComparer(selectedRow, df, prayer_headers, currentMYT)
+currentTime, displayCurrentTime, act_times, iqama_times, display_act_times, display_iqama_times, current_prayer, next_prayer= timeComparer(selectedRow, df, prayer_headers, iqama_headers, currentMYT)
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
@@ -14,7 +16,7 @@ ctk.set_default_color_theme("green")
 root = ctk.CTk()
 
 root.title("Prayer Times - ISOC 24/25")
-root.geometry('1280x720')
+root.geometry('720x1080')
 
 my_image = ctk.CTkImage(light_image=Image.open("Images/ISoc (black).png"),
                                   dark_image=Image.open("Images/ISoc (white).png"),
@@ -29,11 +31,12 @@ label = ctk.CTkLabel(master=frame, text=f"Prayer Times", font=("Roboto", 24))
 label.pack(pady=12, padx=10)
 
 
+
 if foundFlag == True:
     label = ctk.CTkLabel(master=frame, text=f"{date} | {hijri}", font=("Roboto", 48))
     label.pack(pady=12, padx=10)
     label = ctk.CTkLabel(master=frame, text=f"{day}", font=("Roboto", 32))
-    label.pack(pady=12, padx=10)
+    label.pack(pady=6, padx=10)
     # print(f"{date} | {hijri}")
     # print(f"{day}")
 
@@ -41,34 +44,32 @@ if foundFlag == False:
     print("XX:XX - UPDATE REQUIRED")
 
 
-label = ctk.CTkLabel(master=frame, text=f"Current: {displayCurrentTime}", font=("Roboto", 32))
-label.pack(pady=12, padx=10)
-label = ctk.CTkLabel(master=frame, text=f"{current_prayer} is now!", font=("Roboto", 32))
-label.pack(pady=12, padx=10)
-label = ctk.CTkLabel(master=frame, text=f"Next: {display_times[next_prayer]}", font=("Roboto", 32))
-label.pack(pady=12, padx=10)
+currenttime_label = ctk.CTkLabel(master=frame, text=f"{displayCurrentTime}", font=("Roboto", 48))
+currenttime_label.pack(pady=30, padx=10)
+# label = ctk.CTkLabel(master=frame, text=f"{current_prayer} is now!", font=("Roboto", 32))
+# label.pack(pady=12, padx=10)
+# label = ctk.CTkLabel(master=frame, text=f"Next: {display_times[next_prayer]}", font=("Roboto", 32))
+# label.pack(pady=12, padx=10)
 
-for i in range(len(prayer_headers)):  # Iterate over the prayer headers
+prayer_labels = {}
 
-    # next_prayer = prayer_headers[i]
-    # currrent_prayer = prayer_headers[i-1]
+for i in range(len(prayer_headers)):
+    if i == 1:
+        continue  # Skip the element at index 2
+    sel = prayer_headers[i]
+    prayer_labels[i] = ctk.CTkLabel(master=frame, text=f"{prayer_headers[i]} - {display_act_times[sel]} : {display_iqama_times[sel]}", font=("Roboto", 32))
+    prayer_labels[i].pack(pady=12, padx=10)
 
-    if currentTime < act_times[next_prayer]:  # Compare current time with the actual time
-        # print(f"\nCurrent: {displayCurrentTime}")
-        # print(f"{current_prayer} is now!")
-        # print(f"next_prayer: {display_times[next_prayer]}")
-        # print(f"{next_prayer} is next_prayer!")
+if currentTime < act_times[next_prayer]:  # Compare current time with the actual time
 
-        label = ctk.CTkLabel(master=frame, text=f"{next_prayer} is next!", font=("Roboto", 32))
-        label.pack(pady=12, padx=10)
-        break
-    elif currentTime > act_times['Isyak']:
-        # print(f"{current_prayer} is now!")
-        # print(f"next_prayer: {display_times[next_prayer]}")
-        # print(f"Fajr is next_prayer!")
-        label = ctk.CTkLabel(master=frame, text=f"Fajr is next!", font=("Roboto", 32))
-        label.pack(pady=12, padx=10)
-        break
+    label = ctk.CTkLabel(master=frame, text=f"{next_prayer} is next!", font=("Roboto", 32))
+    label.pack(pady=12, padx=10)
+elif currentTime > act_times['Isyak']:
+    # print(f"{current_prayer} is now!")
+    # print(f"next_prayer: {display_times[next_prayer]}")
+    # print(f"Fajr is next_prayer!")
+    label = ctk.CTkLabel(master=frame, text=f"Fajr is next!", font=("Roboto", 32))
+    label.pack(pady=12, padx=10)
 
 # # adding a label to the root window
 # lbl = Label(root, text = "Prayer Times")
--- timeChecker.py
+++ timeChecker.py
@@ -23,9 +23,23 @@ def initializers(offset):
     # Select only the relevant columns
     relevant_columns = ['Date', 'Hijri', 'Day', 'Imsak', 'Fajr', 'Syuruk', 'Zohor', 'Asar', 'Maghrib', 'Isyak']
     prayer_headers = ['Fajr', 'Syuruk', 'Zohor', 'Asar', 'Maghrib', 'Isyak']
+    iqama_headers = [25, 0, 15, 15, 10, 15]
+
+    # Replace the values in the 'Day' column
+    day_mapping = {
+        'Isnin': 'Monday',
+        'Selasa': 'Tuesday',
+        'Rabu': 'Wednesday',
+        'Khamis': 'Thursday',
+        'Jumaat': 'Friday',
+        'Sabtu': 'Saturday',
+        'Ahad': 'Sunday'
+    }
+    df['Day'] = df['Day'].replace(day_mapping)
+
     df = df[relevant_columns]
 
-    return df, prayer_headers, currentMYT
+    return df, prayer_headers, iqama_headers, currentMYT
 
 
 # %%
@@ -39,10 +53,10 @@ def dateComparer(df, prayer_headers, currentMYT):
         i += 1
         if rowDate == currentDate:
             found = True
-            print(f"{rowDate} | {Hijri}")
-            print(f"{Day}")
-            for j in prayer_headers:
-                print(f"{j} - {df.loc[i, f'{j}']}")
+            # print(f"{rowDate} | {Hijri}")
+            # print(f"{Day}")
+            # for j in prayer_headers:
+                # print(f"{j} - {df.loc[i, f'{j}']}")
             selectedRow = i
             break
 
@@ -53,114 +67,52 @@ def dateComparer(df, prayer_headers, currentMYT):
 
 
 # %%
-def timeComparer(selectedRow, df, prayer_headers, currentMYT):
+def timeComparer(selectedRow, df, prayer_headers, iqama_headers, currentMYT):
     currentTime = datetime.time(currentMYT.tm_hour, currentMYT.tm_min, currentMYT.tm_sec)
-    displayCurrentTime = currentTime.strftime('%I:%M %p')  # Format current time as HH:MM:SS AM/PM
+    
+    
+    # Format current time manually to avoid platform-specific issues
+    hour = currentMYT.tm_hour
+    if hour > 12:
+        hour -= 12  # Convert to 12-hour format
+    elif hour == 0:
+        hour = 12  # Handle midnight as 12 AM
+
+    displayCurrentTime = f"{hour}:{currentMYT.tm_min:02}:{currentMYT.tm_sec:02} {'AM' if currentMYT.tm_hour < 12 else 'PM'}"
     
     # Create dictionaries to hold your times and display times
     act_time = {}
-    display = {}
+    iqama_time = {}
+    display_act_time = {}
+    display_iqama_time = {}
 
-    for i in prayer_headers:
+    for j, i in enumerate(prayer_headers):
         # Store the values in dictionaries
-        act_time[i] = datetime.datetime.strptime(df.loc[selectedRow, f'{i}'], '%I:%M %p').time()
-        display[i] = datetime.datetime.strptime(df.loc[selectedRow, f'{i}'], '%I:%M %p').strftime('%I:%M %p')
+        act_time[i] = datetime.datetime.strptime(df.loc[selectedRow, f'{i}'], '%I:%M %p')
+        iqama_time[i] = (act_time[i] + datetime.timedelta(minutes=iqama_headers[j])).time()
+        act_time[i] = act_time[i].time()
+        display_act_time[i] = f"{int(act_time[i].strftime('%I'))}:{act_time[i].strftime('%M %p')}"
+        display_iqama_time[i] = f"{int(iqama_time[i].strftime('%I'))}:{iqama_time[i].strftime('%M %p')}"
+
 
     for i in range(len(prayer_headers)):  # Iterate over the prayer headers
         next = prayer_headers[i]
         curr = prayer_headers[i-1]
         if currentTime < act_time[next]:  # Compare current time with the actual time
-            print(f"\nCurrent: {displayCurrentTime}")
-            print(f"Next: {display[next]}")
-            print(f"{curr} is now!")
-            print(f"{next} is next!")
+            # print(f"\nCurrent: {displayCurrentTime}")
+            # print(f"Next: {display[next]}")
+            # print(f"{curr} is now!")
+            # print(f"{next} is next!")
             break
         elif currentTime > act_time['Isyak']:
-            print(f"\nCurrent: {displayCurrentTime}")
-            print(f"{curr} is now!")
-            print(f"Next: {display[next]}")
-            print(f"Fajr is next!")
+            # print(f"\nCurrent: {displayCurrentTime}")
+            # print(f"{curr} is now!")
+            # print(f"Next: {display[next]}")
+            # print(f"Fajr is next!")
             break
-    return currentTime, displayCurrentTime, act_time, display, curr, next
+    return currentTime, displayCurrentTime, act_time, iqama_time, display_act_time, display_iqama_time, curr, next
 
 # %%
-df, prayer_headers, currentMYT = initializers(8)
+df, prayer_headers, iqama_headers, currentMYT = initializers(8)
 selectedRow, date, hijri, day, headers, foundFlag = dateComparer(df, prayer_headers, currentMYT)
-currentTime, displayCurrentTime, act_times, display_times, current_prayer, next_prayer= timeComparer(selectedRow, df, prayer_headers, currentMYT)
-
-print(selectedRow)
-print(date)
-print(hijri)
-print(day)
-print(headers)
-
-print(currentTime)
-print(displayCurrentTime)
-
-# %%
-
-    # fajrTime = datetime.datetime.strptime(df.loc[selectedRow, 'Fajr'], '%I:%M %p').time()
-    # displayFajrTime = datetime.datetime.strptime(df.loc[selectedRow, 'Fajr'], '%I:%M %p').strftime('%I:%M %p')
-    
-    # syurukTime = datetime.datetime.strptime(df.loc[selectedRow, 'Syuruk'], '%I:%M %p').time()
-    # displaySyurukTime = datetime.datetime.strptime(df.loc[selectedRow, 'Syuruk'], '%I:%M %p').strftime('%I:%M %p')
-    
-    # zohorTime = datetime.datetime.strptime(df.loc[selectedRow, 'Zohor'], '%I:%M %p').time()
-    # displayZohorTime = datetime.datetime.strptime(df.loc[selectedRow, 'Zohor'], '%I:%M %p').strftime('%I:%M %p')
-    
-    # asarTime = datetime.datetime.strptime(df.loc[selectedRow, 'Asar'], '%I:%M %p').time()
-    # displayAsarTime = datetime.datetime.strptime(df.loc[selectedRow, 'Asar'], '%I:%M %p').strftime('%I:%M %p')
-    
-    # maghribTime = datetime.datetime.strptime(df.loc[selectedRow, 'Maghrib'], '%I:%M %p').time()
-    # displayMaghribTime = datetime.datetime.strptime(df.loc[selectedRow, 'Maghrib'], '%I:%M %p').strftime('%I:%M %p')
-    
-    # isyakTime = datetime.datetime.strptime(df.loc[selectedRow, 'Isyak'], '%I:%M %p').time()
-    # displayIsyakTime = datetime.datetime.strptime(df.loc[selectedRow, 'Isyak'], '%I:%M %p').strftime('%I:%M %p')
-    # if currentTime < syurukTime:
-    #     print(f"{displayCurrentTime} vs {displaySyurukTime}")
-    #     print("FAJR RIGHT NOW!")
-    # elif currentTime < zohorTime:
-    #     print(f"{displayCurrentTime} vs {displayZohorTime}")
-    #     print("NOTHING RIGHT NOW")
-
-    # elif currentTime < asarTime:
-    #     print(f"{displayCurrentTime} vs {displayAsarTime}")
-    #     print("ZOHOR RIGHT NOW")
-
-    # elif currentTime < maghribTime:
-    #     print(f"{displayCurrentTime} vs {displayMaghribTime}")
-    #     print("ASAR RIGHT NOW")
-
-    # elif currentTime < isyakTime:
-    #     print(f"{displayCurrentTime} vs {displayIsyakTime}")
-    #     print("MAGHRIB RIGHT NOW")
-
-    # elif currentTime < fajrTime:
-    #     print(f"{displayCurrentTime} vs {displayFajrTime}")
-    #     print("ISYAK RIGHT NOW")
-        
-
-        # parsedRowDate = time.strptime(f'{rowDate}', '%d-%b-%y')
-
-
-# def constantClock():
-#     while True:
-#       currentTimeUTC = time.gmtime()  # Get current time in UTC
-#       currentTimeMYT = time.localtime(time.mktime(currentTimeUTC) + 8 * 3600)  # Add 8 hours for GMT+8
-#       time.sleep(5)
-# constantClock()
-    # print(currentRowData.to_dict()) <--- this line would make all the rows appear as a dict if we were to include all columns, more readable
-    # if rowDate == CurrentDate:
-    #     selectedRow = i
-    #     break
-
-# if currentTime == (before)row.Date.Syuruk, print("FAJR + RIGHT NOW")
-# else if currentTime == (before)row.Date.Dhuhr, print("NOTHING + RIGHT NOW")
-# else if currentTime == (before)row.Date.Asr, print("DHUHR + RIGHT NOW")
-# else if currentTime == (before)row.Date.Maghrib, print("ASR + RIGHT NOW")
-# else if currentTime == (before)row.Date.Isyak, print("MAGHRIB + RIGHT NOW")
-# else if currentTime == (before)row.Date.Fajr, print("ISYAK + RIGHT NOW")
-
-# for every 5 seconds, print("Current Time - XX:XX Z") # CLOCK CONSTANTLY RUNNING
-
-
+currentTime, displayCurrentTime, act_times, iqama_times, display_act_times, display_iqama_times, current_prayer, next_prayer= timeComparer(selectedRow, df, prayer_headers, iqama_headers, currentMYT)
